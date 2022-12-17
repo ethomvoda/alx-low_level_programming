7bf8dd19c71621684453500069717fad51f7f483
Ernest Thompson <eothompson@st.ug.edu.gh>
2022-12-17 13:06:16 -0800

    super timred

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: 'masa masa'
- *
- * Return: 0 (Success)
- */
-
 int main(void)
 
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n	n";
-	long l = 59;
 
-	long fd = 1;
+		char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	long syscall = 1;
+		long l = 59;
 
-	long ret = 0;
+		long fd = 1;
+
+		long syscall = 1;
+
+		long ret = 0;
 
 
 
-	__asm__ ("syscall"
-			: "=a" (ret)
+		__asm__ ("syscall"
 
-			: "a" (syscall), 
-				"D" (fd),
+			        : "=a" (ret)
+
+				: "a" (syscall),														"D" (fd),
 
 		          	"S" (s),
-														"d" (1))
-		);
-	return (1);
+																				"d" (l));
+		return (1);
 
 }
