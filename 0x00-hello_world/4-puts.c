cd7448075720604d44c376d067c48d0c92d78a93
Ernest Thompson <eothompson@st.ug.edu.gh>
2022-12-17 12:00:26 -0800

    see

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Description: 'prints something'
+ *
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
