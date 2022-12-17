b7ed2f0f8c6a2c5f70f42e3991654024f9f0dddb
Ernest Thompson <eothompson@st.ug.edu.gh>
2022-12-17 12:05:19 -0800

    catching up

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print with printf'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
