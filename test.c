bb0ec5bbe0ba230d0f8db56218ecee05e16c801e
Ernest Thompson <eothompson@st.ug.edu.gh>
2022-12-19 03:32:52 -0800

    showed some skills

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'Program should do that'
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	for (a = 0; a < b; a++)
+		printf("%d\n", a);	
+	return (0);
+}
