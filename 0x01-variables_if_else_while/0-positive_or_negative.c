45a006cfdb0ded246db02633e2d894fb993a164d
Ernest Thompson <eothompson@st.ug.edu.gh>
2022-12-19 03:41:59 -0800

    charlee

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,12 +18,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n < 2)
+	if (n < 2)
 	{
 		printf("%d is negative\n", n);
 	}
 	else 
-		if(n = 0)
+		if (n = 0)
 		{
 			printf("%d is zero\n", n);
 		}
