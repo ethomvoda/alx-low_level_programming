025bcfb22e817eed98318310aaf57a810783063d
Ernest Thompson <eothompson@st.ug.edu.gh>
2023-01-03 02:47:04 -0800

    see me

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print alphabets'
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch='a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+
+		ch++;
+	}
+
+	_putchar('\n');
+
+}
