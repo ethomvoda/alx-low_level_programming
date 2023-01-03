4c1cc72ff753bafd6f91012340bcd190627d3c24
Ernest Thompson <eothompson@st.ug.edu.gh>
2023-01-03 05:20:48 -0800

    youd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * main - Entry point
+ *
+ * Description: 'Print alphabets 10times'
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	unsigned int i;
+	char ch;
+
+	for (i = 0; i < 10, i++)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}
