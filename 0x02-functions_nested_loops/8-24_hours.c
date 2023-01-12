9851fb384367d23c1e620d5d0558398a8a841dc8
Ernest Thompson <eothompson@st.ug.edu.gh>
2023-01-12 11:31:02 -0800

    verily

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,5 @@ int _isalpha(int);
 int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
-
+void jack_bauer(void);
 #endif
