c17ffedcc40d607b1090cb90315e807eec7aa028
Ernest Thompson <eothompson@st.ug.edu.gh>
2023-01-12 11:59:37 -0800

    wheeww

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: integers sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
