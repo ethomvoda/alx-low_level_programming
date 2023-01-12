79b47005877b3c68322ab0d62e4d56ad3c3cad1a
Ernest Thompson <eothompson@st.ug.edu.gh>
2023-01-12 11:45:58 -0800

    wheew

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: none
+ *
+ */
+
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; mult <= 9; mult++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
