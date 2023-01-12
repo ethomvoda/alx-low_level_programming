67232f00dd4b0db608910c5d00da23d0e54b9810
Ernest Thompson <eothompson@st.ug.edu.gh>
2023-01-12 10:17:18 -0800

    wheew

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs- calculates absolute
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
